6c5da239268ee15338d85aae551e59228695cc43
Adityasputra <aditsputra.cv@gmail.com>
2024-08-07 01:33:17 +0700

    Feat: Create Add Coins in CoinController

--- server/controllers/coinController.js
+++ server/controllers/coinController.js
@@ -1,3 +1,27 @@
+const { Coin } = require("../models");
+const { fetchCoinData, fetchCoinList } = require("../services/coinService");
+
 module.exports = class CoinController {
-    
+  static async createCoin(req, res) {
+    try {
+      const coins = await Coin.create(req.body);
+      res.status(201).json(coins);
+    } catch (error) {
+      if (
+        error.name === "SequelizeValidationError" ||
+        error.name === "SequelizeUniqueConstraintError"
+      ) {
+        const validate = error.errors.map((e) => message);
+        res.status(400).json({
+          message: validate,
+        });
+      } else {
+        res.status(500).json({
+          message: "Internal Server Error",
+        });
+      }
+    }
+  }
+
+  
 };
