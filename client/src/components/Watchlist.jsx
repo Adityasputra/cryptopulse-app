0d43a4e155ed5d664633a92b069fa86e0c8fe34a
Adityasputra <aditsputra.cv@gmail.com>
2024-08-08 07:35:12 +0700

    Feat: Create Components Watchlist

--- client/src/components/Watchlist.jsx
+++ client/src/components/Watchlist.jsx
@@ -1,5 +1,19 @@
-export default function Watchlist(){
-    return (
-        <></>
-    )
+export default function Watchlist() {
+  return (
+    <>
+      <div className="bg-gray-800 p-4 rounded-lg shadow-md">
+        <h2 className="text-lg font-bold">Watchlist</h2>
+        <div className="mt-4">
+          <div className="flex justify-between">
+            <span>Bitcoin</span>
+            <span>$43,577.00</span>
+          </div>
+          <div className="flex justify-between mt-2">
+            <span>Ethereum</span>
+            <span>$43,977.00</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
